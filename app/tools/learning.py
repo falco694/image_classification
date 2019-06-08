b81d05deaaadb5048af65054a572c85eecd03646
falcon694 <falcon694@gmail.com>
2019-06-08 19:33:57 +0900

    clean format.

--- app/tools/learning.py
+++ app/tools/learning.py
@@ -5,6 +5,8 @@
 kerasでの画像分類の学習
 """
 
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
 from keras.applications.vgg16 import VGG16
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from keras.datasets import mnist
@@ -24,14 +26,14 @@ from sklearn.model_selection import train_test_split
 # 各種データ
 x = []
 y = []
-dictlabel = {} # ラベル格納
-width = 128 # 立幅
-side = 128 # 横幅
+dictlabel = {}  # ラベル格納
+width = 128  # 立幅
+side = 128  # 横幅
 # color = 3 # モノクロ:1,カラー:3
-test_size = 0.7 # 訓練データの割合
-datadir = "dataset/" # データ格納先
-batch_size = 32 # 学習毎のデータ数
-epoch = 40 # 学習回数
+test_size = 0.7  # 訓練データの割合
+datadir = "dataset/"  # データ格納先
+batch_size = 32  # 学習毎のデータ数
+epoch = 40  # 学習回数
 
 # 学習用のデータを読み込む。
 # datadir以下の画像を読み込む。
@@ -73,23 +75,23 @@ x = np.array(x)
 y = to_categorical(y)
 
 # ランダムにシャッフルし、学習データと検証データに振り分け
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=train_size)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size=train_size)
 
-### add for TensorBoard
-import keras.backend.tensorflow_backend as KTF
-import tensorflow as tf
+# add for TensorBoard
 
 old_session = KTF.get_session()
 
 session = tf.Session('')
 KTF.set_session(session)
 KTF.set_learning_phase(1)
-### 
+###
 
 # モデル構築
 # 転移学習
 input_tensor = Input(shape=(width, side, 3))
-base_model = VGG16(weights='imagenet', include_top=False,input_tensor=input_tensor)
+base_model = VGG16(weights='imagenet', include_top=False,
+                   input_tensor=input_tensor)
 
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
@@ -112,30 +114,33 @@ for layer in base_model.layers[:15]:
 
 # モデルコンパイル
 # model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
-model.compile(loss="categorical_crossentropy", optimizer='adadelta', metrics=["accuracy"])
+model.compile(loss="categorical_crossentropy",
+              optimizer='adadelta', metrics=["accuracy"])
 # モデルを表示
 model.summary()
 
 # 学習
 open("model", "w").write(model.to_json())
-### add for TensorBoard
+# add for TensorBoard
 log_filepath = "./logs/"
-tb_cb = TensorBoard(log_dir=log_filepath, histogram_freq=1, write_graph=True, write_images=True)
+tb_cb = TensorBoard(log_dir=log_filepath, histogram_freq=1,
+                    write_graph=True, write_images=True)
 ###
 check = ModelCheckpoint("model.hdf5", save_best_only="true")
-earlystop = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='auto')
+earlystop = EarlyStopping(
+    monitor='val_loss', patience=10, verbose=0, mode='auto')
 callbacks = [check, earlystop, tb_cb]
 # 指定した割合に応じて、trainとtestデータに振り分ける。
 history = model.fit(x, y,
-          batch_size=batch_size,
-          epochs=epoch,
-          verbose=1,
-          callbacks = callbacks,
-          validation_split=0.3)
+                    batch_size=batch_size,
+                    epochs=epoch,
+                    verbose=1,
+                    callbacks=callbacks,
+                    validation_split=0.3)
 
 # 学習状況を表示
 loss_and_metrics = model.evaluate(x_test, y_test, verbose=0)
-print("\nloss:{} accuracy:{}".format(loss_and_metrics[0],loss_and_metrics[1]))
+print("\nloss:{} accuracy:{}".format(loss_and_metrics[0], loss_and_metrics[1]))
 
 # モデルを写真形式で保存
 # エラーとなるため、無効化している。
@@ -167,7 +172,8 @@ print("\nloss:{} accuracy:{}".format(loss_and_metrics[0],loss_and_metrics[1]))
 # plot_history(history)
 
 fig = plt.figure(figsize=(2, 9))
-fig.subplots_adjust(left=0, right=1, bottom=0, top=0.5, hspace=0.05, wspace=0.05)
+fig.subplots_adjust(left=0, right=1, bottom=0,
+                    top=0.5, hspace=0.05, wspace=0.05)
 for i in range(18):
     ax = fig.add_subplot(2, 9, i + 1, xticks=[], yticks=[])
     ax.imshow(x_train[i].reshape((128, 128)), cmap='rgb')
